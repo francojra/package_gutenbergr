31acca2b8d77db413fc5fcc6cfdb9c812f38ab73
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-06 00:08:46 -0300

    Add files via upload

--- package_gutenbergr.R
+++ package_gutenbergr.R
@@ -40,3 +40,11 @@ gutenberg_subjects %>%
 gutenberg_subjects %>%
   filter(grepl("Handbooks, manuals, etc.", subject)) %>%
   view()
+
+# Baixando livros --------------------------------------------------------------------------------------------------------------------------
+
+pride <- gutenberg_download(1342)
+view(pride)
+
+books <- gutenberg_download(c(2837, 1342), meta_fields = c("title", "author"))
+view(books) # Baixa dois livros
